b3dd90c23f043c9f87a1bb827e0762e87f3c1f1b
NoOneLevi <zxvitor1@hotmail.com>
2023-11-22 12:47:58 -0300

    change the endpoint of the api

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const sumComponent = document.querySelector('.sumNumber')
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
     if(inputNumber.value) {
-        const data = await fetch('https://desafio-t-cnico.vercel.app/number', {
+        const data = await fetch('https://desafio-tecnico-gamma.vercel.app/number', {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
